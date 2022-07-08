cf2a571cbdb0c2256dee6b170ee78413c906e6d2
Kevin Falcoz <0pc0defr@gmail.com>
2022-07-08 16:12:00 +0200

    Create crypto module

---
+++ always_cms/libs/crypto/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Cryptographic module used for secure Always-CMS"""
+
+from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+from Crypto import Random
+from hashlib import sha256
+
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+def encrypt(raw, password):
+    private_key = hashlib.sha256(password.encode("utf-8")).digest()
+    raw = pad(raw)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(raw))
+
+
+def decrypt(enc, password):
+    private_key = hashlib.sha256(password.encode("utf-8")).digest()
+    enc = base64.b64decode(enc)
+    iv = enc[:16]
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[16:]))
